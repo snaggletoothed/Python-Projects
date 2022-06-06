55641bf57ad0e63eb312ab96a2551d13758fdc98
snaggletoothed <pretorius.meh.gareth@gmail.com>
2022-06-06 23:55:46 +0200

    remembered old project

--- character_generator/character_generator.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('SSA_Names_DB.csv')
-
-print(df.to_string()) 
---
+++ character_generator/name_creator.py
@@ -0,0 +1,24 @@
+import pandas as pd, random
+#import csv and converts it to dataframe
+df = pd.read_csv('/home/snaggletoothed/Documents/Python-Projects/character_generator/names.csv')
+#create lists to serepate csv columns
+list_male = df['MALE'].tolist() 
+list_female = df['FEMALE'].tolist()
+surname_list = df['SURNAMES'].tolist()
+
+#func to assign male names
+def male_name():
+    x = random.randint(1,6288)
+    name = list_male[x]
+    x = random.randint(1,10443)
+    surname = surname_list[x]
+    male_name = name + ' ' + surname
+    return male_name
+#func to assisgn female names
+def female_name():
+    x = random.randint(1,4391)
+    name = list_female[x]
+    x = random.randint(1,10443)
+    surname = surname_list[x]
+    female_name = name + ' ' + surname
+    return female_name  
